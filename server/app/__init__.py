e8fd570ebfd9ca6dd31b1a17a854659fcc32842c
Cenagaurav77 <sinhagaurav7788@gmail.com>
2024-07-27 15:41:50 -0400

    Swagger Documentation added

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 from app.middleware.error_handler import register_error_handlers
 
 def create_app():
@@ -13,5 +14,11 @@ def create_app():
     # Import and register the company blueprint for route handling
     from app.routes.company_routes import company_bp
     app.register_blueprint(company_bp)
+
+    # Swagger UI configuration
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.yaml'
+    swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': "Company Listings API"})
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     return app
