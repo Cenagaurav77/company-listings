d38317c36625ae38590f88b8e55c300cfc87bd0f
Cenagaurav77 <sinhagaurav7788@gmail.com>
2024-07-26 22:08:03 -0400

    made website resposnive

--- client/src/components/Menu/index.jsx
+++ client/src/components/Menu/index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const DropdownMenu = ({ locations, onLocationChange }) => {
   return (
-    <div className="dropdown-container mt-4">
+    <div className="dropdown-container mt-4 max-w-xs mx-auto">
       <select
         onChange={(e) => onLocationChange(e.target.value)}
-        className="dropdown-select p-2 border rounded"
+        className="dropdown-select p-2 border rounded w-full"
       >
         {locations.map((location) => (
           <option key={location.location_id} value={location.location_id}>
--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div>
-      <nav className="flex items-center justify-between h-20 max-w-6xl mx-auto">
+      <nav className="flex items-center justify-between h-20 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <Link to={"/"}>
           <div className="ml-5">
             <h1 className="text-red-900 font-bold text-xl sm:text-2xl md:text-3xl cursor-pointer tracking-wide">
